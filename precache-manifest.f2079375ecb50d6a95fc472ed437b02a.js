7b810b2d1a9f8f505e41c7a2a379b85165186160
Ivlev Michael <ivle.michael@gmail.com>
2019-09-21 23:45:54 +0300

    Updates

---
+++ precache-manifest.f2079375ecb50d6a95fc472ed437b02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345ca5f584ff3d6350a69fe161ec8a62",
+    "url": "/React-mutant/index.html"
+  },
+  {
+    "revision": "ac6884ec9104d7229f1f",
+    "url": "/React-mutant/static/css/main.a619002f.chunk.css"
+  },
+  {
+    "revision": "370b5117c219b10b6720",
+    "url": "/React-mutant/static/js/2.7f4e1161.chunk.js"
+  },
+  {
+    "revision": "ac6884ec9104d7229f1f",
+    "url": "/React-mutant/static/js/main.089b2cea.chunk.js"
+  },
+  {
+    "revision": "3a00c2ca3132bdafccb6",
+    "url": "/React-mutant/static/js/runtime~main.1788a067.js"
+  },
+  {
+    "revision": "492c4649d9e23b152eed03d089a4ef93",
+    "url": "/React-mutant/static/media/bg-pt.492c4649.jpg"
+  },
+  {
+    "revision": "99bf3227a5603a478cfbf6682f7abb72",
+    "url": "/React-mutant/static/media/crumbaugh.99bf3227.jpg"
+  }
+]);
